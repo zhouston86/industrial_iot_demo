9edb2f63277c6df8b6b1f8ff0ea816005d08c6aa
zhouston86 <116024224+zhouston86@users.noreply.github.com>
2022-12-26 13:51:53 -0600

    added ignition backups

--- ignition_tags/create_reference_tags.py
+++ ignition_tags/create_reference_tags.py
@@ -1,9 +1,25 @@
+#TODO: only add tags in nam_mapping.csv
+#TODO: 
+
+
 import json
 import os
+import csv
 
 
 f = open('ignition_tags/tag_export.json')
 io_tags = json.load(f)
+
+csv_dict = {}
+
+with open('ignition_tags/manual_name_mapping.csv') as csv_f:
+  csv_reader = csv.DictReader(csv_f)
+  for row in csv_reader:
+    print(row)
+    csv_dict[row['io']] = row['name']
+
+#print(csv_dict.keys())
+    
 
 if os.path.exists('ignition_tags/reference_import.json'):
     os.remove('ignition_tags/reference_import.json')
@@ -14,14 +30,19 @@ tag_dict_list = []
 
 
 for i in io_tags['tags'][1]['tags']:
+    # ignore ignition system tags that start with "_"
     if i['name'][0] != '_':
-        new_dict_entry = {}
+      new_dict_entry = {}
 
+      tag_lookup_name = str(i['name']).replace("_","/")
+      #print(tag_lookup_name)
 
+      if tag_lookup_name in csv_dict.keys():
+          
         new_dict_entry["valueSource"] = "reference"
         new_dict_entry["tagType"] = "AtomicTag"
         new_dict_entry["dataType"] = i['dataType']
-        new_dict_entry["name"] = i['name']
+        new_dict_entry["name"] = csv_dict[tag_lookup_name]
         new_dict_entry["sourceTagPath"] = "[~]Device1/{0}".format(i['name']) + ".value"
 
         tag_dict_list.append(new_dict_entry)
@@ -36,6 +57,6 @@ json_export = {
       "tags": tag_dict_list 
     }]}
 
-print(json.dumps(json_export,indent=4))
+#print(json.dumps(json_export,indent=4))
 
 fout.write(json.dumps(json_export,indent=4))
